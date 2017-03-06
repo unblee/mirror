eece785ec5ff20d08109db1610c3a63eb58d4799
unblee <unblee@users.noreply.github.com>
2017-03-06 23:53:13 +0900

    Add example of environment values

--- mirror.go
+++ mirror.go
@@ -30,24 +30,32 @@ Flags:
 Available Environment Values:
   LISTEN_PORT              Listening port number
     default: 8080
+    e.g.) LISTEN_PORT=8080
 
   DEFAULT_DEST_PORT        Default destination port number
     default: 80
+    e.g.) DEFAULT_DEST_PORT=80
 
   BASE_DOMAIN              Base Domain
     default: not set(required)
+    e.g.) BASE_DOMAIN=127.0.0.1.xip.io
+    e.g.) BASE_DOMAIN=example.com
 
   DEFAULT_DEST_URL         Default destination URL
     default: not set(allow empty)
+    e.g.) DEFAULT_DEST_URL=http://127.0.0.1:5000
 
   DB_HOST                  Hostname of the database to connect
     default: 127.0.0.1
+    e.g.) DB_HOST=127.0.0.1
 
   DB_PORT                  Port number of the database to connect
     default: 6379
+    e.g.) DB_PORT=6379
 
   REDIS_HASH_KEY           Hash key of Redis
     default: mirror-store
+    e.g.) REDIS_HASH_KEY=mirror-store
 `
 
 const (
